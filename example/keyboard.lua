79e483116ef2e8e3da69131ccb81d927e7e443b8
Bouchard Justin <79466202+justdie386@users.noreply.github.com>
2023-06-11 21:03:20 -0400

    Update keyboard.lua

--- example/keyboard.lua
+++ example/keyboard.lua
@@ -5,7 +5,7 @@ print("cool isn't it?")
 end) --will register an event that will occur when the key is pressed
 keyboard.register(0x002E, function()
     print("cool isn't it?")
-    end) --the point of this system is to make so you can listen to multiple keys
+end) --the point of this system is to make so you can listen to multiple keys
 
 keyboard.run() --will start listening to the previous keys event
 
