24d97b66ce7e9cb5594b0852eef294aeb0077f90
Bouchard Justin <79466202+justdie386@users.noreply.github.com>
2023-06-17 10:05:01 -0400

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -15,5 +15,3 @@ cd lua-5.1.5 && .\Compile.bat && cd ..
 gcc keyboard.c -fPIC -Wall -shared -o keyboard.dll ./lua-5.1.5/src/lua.lib ./uiohook.lib
 echo I highly recommend you use the lua.exe file built by this script as it will not have any architecture issues with the modules
 copy "lua5.1.5/src/lua.exe" . /y
-rmdir lua5.1.5 /s
-rmdir libuiohook /s
