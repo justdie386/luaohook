a40a5ba95f15f3526b5173f2356965c244460061
Bouchard Justin <79466202+justdie386@users.noreply.github.com>
2023-07-03 16:02:46 +0000

    Update linux.sh

--- install-script/linux.sh
+++ install-script/linux.sh
@@ -1,11 +1,11 @@
  git clone https://github.com/justdie386/luaohook
- cd luaohook
+ cd luaohook;cd src
  git clone https://github.com/kwhat/libuiohook
  cd libuiohook
  mkdir build && cd build
  cmake -S .. -D BUILD_SHARED_LIBS=ON -D BUILD_DEMO=ON -DCMAKE_INSTALL_PREFIX=../dist
  cmake --build . --parallel 2 --target install
- cd ..
+ cd ..; cd ..
  cp libuiohook/build/libuiohook.so .
  sudo cp libuiohook/build/libuiohook.so.1 /usr/lib
  cd src
