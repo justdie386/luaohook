ad5da7faa742574f276befa19c9eaa8507b08ebc
Bouchard Justin <79466202+justdie386@users.noreply.github.com>
2023-07-03 16:03:28 +0000

    Update macos.sh

--- install-script/macos.sh
+++ install-script/macos.sh
@@ -1,5 +1,6 @@
  git clone https://github.com/justdie386/luaohook
  cd luaohook
+ src src
  git clone https://github.com/kwhat/libuiohook
  cd libuiohook
  mkdir build && cd build
@@ -8,7 +9,6 @@
  cd ..; cd ..
  cp libuiohook/build/libuiohook.dylib .
  cp libuiohook/build/libuiohook.1.dylib /usr/local/lib/
- cd src
 gcc -fPIC -Wall -shared  press.c -o press.dylib -llua5.1 -L. -luiohook
 gcc -fPIC -Wall -shared  keyboard.c -o keyboard.dylib -llua5.1 -L. -luiohook
 gcc -fPIC -Wall -shared mouse.c -o mouse.so -llua5.1 -L. -luiohook
