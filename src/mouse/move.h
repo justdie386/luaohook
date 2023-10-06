0813843e9f8fc334e4d875d6179e9dbba9ba10c2
Bouchard Justin <79466202+justdie386@users.noreply.github.com>
2023-10-05 20:34:40 -0400

    Update move.h

--- src/mouse/move.h
+++ src/mouse/move.h
@@ -1,22 +1,5 @@
 #ifndef MOVE_H_
 #define MOVE_H_
-#ifdef _WIN32
-#include <windows.h>
-#include <winuser.h>
-#include "win32/win32.h"
-#define coords POINT
-#elif __APPLE__
-#include <unistd.h>
-#include "osx/osx.h"
-#define coords CGPoint
-#else
-#include <unistd.h>
-#include "x11/x11.h"
-#define coords MouseCoordinates
-#endif
-#include <uiohook.h>
-#include <lauxlib.h>
-#include <lua.h>
 
 int move();
 
